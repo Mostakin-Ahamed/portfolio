8818928b88967574bb220bdd5d81ce3afa49a13d
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2023-12-17 00:30:05 +0600

    navbar button fixed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     return (
         <div>
 
-            <div className="navbar fixed bg-base-100">
-                <div className="navbar-start">
+            <div className="navbar bg-base-100 border border-red-500">
+                <div className="navbar-start ">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -15,17 +15,17 @@ const Navbar = () => {
                             <Link to={'/home'}><li><a>Home </a></li></Link>
                             <Link to={'/aboutMe'}><li><a>About Me </a></li></Link>
                             <Link to={'/skills'}><li><a>Skills </a></li></Link>
-                            <Link to={'/contact'}><li><a>Contact </a></li></Link>
+                            <Link to={'/contact'}><li><a>Contact Me</a></li></Link>
                         </ul>
                     </div>
                     <a className="btn btn-ghost text-xl">Mostakin Ahamed</a>
                 </div>
-                <div className="navbar-center hidden lg:flex ml-10">
+                <div className="navbar-end hidden lg:mr-32 lg:flex border border-black">
                     <ul className="menu menu-horizontal px-1">
                         <Link to={'/home'}><li><a>Home </a></li></Link>
                         <Link to={'/aboutMe'}><li><a>About </a></li></Link>
                         <Link to={'/skills'}><li><a>Skills </a></li></Link>
-                        <Link to={'/contact'}><li><a>Contact </a></li></Link>
+                        <Link to={'/contact'}><li><a>Contact Me</a></li></Link>
                     </ul>
                 </div>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import router from './Routes/Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='w-10/12 mx-auto'>
+    <div className='w-9/12 mx-auto' >
       <RouterProvider router={router} />
     </div>
   </React.StrictMode>,
